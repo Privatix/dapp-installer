b7f776b0d27cfa76a8b63059634af25775fdbc7b
Urmat Bozov <urmat@privatix.io>
2018-08-23 16:51:02 +0600

    check system prerequisites

--- util/misc.go
+++ util/misc.go
@@ -1,29 +1,8 @@
 package util
 
-import (
-	"encoding/json"
-	"os"
+const (
+	// MinAvailableDiskSize - available min 500MB
+	MinAvailableDiskSize uint64 = 500 * 1024 * 1024
+	// MinMemorySize  - min RAM 2 GB
+	MinMemorySize uint64 = 2 * 1000 * 1024 * 1024
 )
-
-// Recursivly remove all data from directory
-func ClearDirectory(path string) {
-	if err := os.RemoveAll(path); err != nil {
-		panic(err)
-	}
-}
-
-// ReadJSONFile reads and parses a JSON file filling a given data instance.
-func ReadJSONFile(name string, data interface{}) error {
-	file, err := os.Open(name)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	return json.NewDecoder(file).Decode(data)
-}
-
-// Version get installer versions
-func Version() string {
-	return "0.0.0.0"
-}

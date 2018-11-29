bf118da4a9760acc295206c9cf73c5956c209aea
drew2a <for.drew@gmail.com>
2018-11-29 13:06:50 +0300

    fix ./

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,12 +8,12 @@ if [ ! -f "${GOPATH}"/bin/dep ]; then
 fi
 echo running dep ensure
 cd "${DAPPINST_DIR}" && dep ensure
-go get -d ./...
+go get -d ${DAPPINST_DIR}/...
 go get -u github.com/rakyll/statik
 go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 go get -u gopkg.in/reform.v1/reform
 
-go generate ./...
+go generate ${DAPPINST_DIR}/...
 
 GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_RELEASE=$(git tag -l --points-at HEAD)

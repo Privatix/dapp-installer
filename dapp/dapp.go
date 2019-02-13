05d23ead3ec61a3803d02df723309037e3db3ec4
Urmat Bozov <urmat@privatix.io>
2019-02-13 22:34:36 +0600

    fix clear gui storage on Windows

--- dapp/dapp.go
+++ dapp/dapp.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"os/user"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -211,24 +210,7 @@ func (d *Dapp) modifyDappConfig() error {
 
 // Remove removes installed dapp core.
 func (d *Dapp) Remove() error {
-	u, err := user.Current()
-	if err != nil {
-		return err
-	}
-	path := u.HomeDir
-	switch runtime.GOOS {
-	case "darwin":
-		path = filepath.Join(path, "Library", "Application Support",
-			"dappctrlgui")
-	case "linux":
-		path = filepath.Join(path, ".config", "dappctrlgui")
-	case "windows":
-		path = filepath.Join(path, "AppData", "Roaming", "dappctrlgui")
-	default:
-		return fmt.Errorf("unsupported OS")
-	}
-
-	if err := os.RemoveAll(path); err != nil {
+	if err := clearGuiStorage(); err != nil {
 		return err
 	}
 

5d86877298eb131a363b2e76f91ec70a610b0ce6
drew2a <for.drew@gmail.com>
2019-06-28 10:28:18 +0300

    improve get_transactions

--- scripts/autooffer/mainnet/get_transactions.py
+++ scripts/autooffer/mainnet/get_transactions.py
@@ -4,9 +4,9 @@ token = get_token(default_password)
 
 accounts = get_accounts(token)
 for account in accounts:
-    transactions = get_eth_transactions(token, "account", account["id"], 0, 100)
-    print("-" * 80)
+    transactions = get_eth_transactions(token, "accountAggregated", account["id"], 0, 100)
     for transaction in transactions:
+        print("-" * 80)
         print("\n{}:\n\t{} {}\n\thttps://etherscan.io/tx/0x{}".format(
             transaction["method"],
             transaction["status"],
